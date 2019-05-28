19410f33d5b7f5cea690d70fadbf0944c60d3424
Dmitriy <kasar@meta.ua>
2019-05-28 20:44:06 +0300

    First commit - 123

--- iacilenisProj/src/test/java/mainPack/AppTest.java
+++ iacilenisProj/src/test/java/mainPack/AppTest.java
@@ -5,8 +5,9 @@ import static org.junit.Assert.assertTrue;
 import org.junit.Test;
 
 /**
- * Unit test for simple App.
- */
+ * Unit test for simple App.123
+ *
+ **/
 public class AppTest 
 {
     /**
