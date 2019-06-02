c74f2ccf75875674f27bf6989b5b73b31a215669
User <Malbox@i.ua>
2019-06-02 19:20:07 +0300

    new comment2 hw9

--- oleinikProj/src/test/java/mainPack/AppTest.java
+++ oleinikProj/src/test/java/mainPack/AppTest.java
@@ -5,7 +5,7 @@ import static org.junit.Assert.assertTrue;
 import org.junit.Test;
 
 /**
- * Unit test for simple App. new comments
+ * Unit test for simple App. new comments/ new comments 2
  */
 public class AppTest 
 {
