8a8d09b7d894bcb5511eb22c083f5c87fd61637f
Maria <mashakoreniuk@gmail.com>
2019-06-09 15:24:08 +0300

    test comit

--- korenukProj/src/test/java/mainPack/AppTest.java
+++ korenukProj/src/test/java/mainPack/AppTest.java
@@ -5,9 +5,9 @@ import static org.junit.Assert.assertTrue;
 import org.junit.Test;
 
 /**
- * Unit test for simple App.
+ * Unit test for simple App.11
  */
-public class AppTest 
+public class AppTest
 {
     /**
      * Rigorous Test :-)
