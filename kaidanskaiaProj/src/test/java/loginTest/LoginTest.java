6bb0e6b5c809173f137974a658b58076b8228465
VarvaraKeid <varvarakeyd@gmail.com>
2019-06-13 21:37:11 +0300

    LoginTest

--- kaidanskaiaProj/src/test/java/loginTest/LoginTest.java
+++ kaidanskaiaProj/src/test/java/loginTest/LoginTest.java
@@ -1,39 +1,62 @@
 package loginTest;
 
+import org.junit.After;
 import org.junit.Assert;
+import org.junit.Before;
 import org.junit.Test;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import pages.HomePage;
 
 import java.io.File;
 import java.util.concurrent.TimeUnit;
 
 public class LoginTest {
-    @Test
-    public void validLogIn(){
+
+    WebDriver webDriver;
+
+    @Before
+    public void setUp(){
         File file = new File("./src/old_drivers/chromedriver.exe");
         System.setProperty("webdriver.chrome.driver", file.getAbsolutePath());
 
-        WebDriver webDriver = new ChromeDriver();
+        webDriver = new ChromeDriver();
 
         webDriver.manage().window().maximize();
         webDriver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+
+    }
+    @Test
+    public void validLogIn() {
+
 
         webDriver.get("http://v3.test.itpmgroup.com");
 
         webDriver.findElement(By.name("_username")).clear();
         webDriver.findElement(By.name("_username")).sendKeys("Student");
 
-        webDriver.findElement( By.id("password")).clear();
+        webDriver.findElement(By.id("password")).clear();
         webDriver.findElement(By.id("password")).sendKeys("909090");
 
         webDriver.findElement(By.tagName("button")).click();
 
+        HomePage homePage = new HomePage(webDriver);
+
+
         Assert.assertTrue("Avatar is not present",
-                webDriver.findElement(By.xpath(".//*[@class='pull-left image']")).isDisplayed());
-        webDriver.quit();
+                //  webDriver.findElement(By.xpath(".//*[@class='pull-left image']")).isDisplayed()
+                homePage.isAvatarPresent());
 
 
     }
+
+    @After
+    public void
+    tearDown(){
+        webDriver.quit();
+    }
+
 }
+
+
