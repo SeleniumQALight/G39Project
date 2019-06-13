6952bd5a0560965ef8ecf3a3f01b806305ce4880
Dinamovec <a.si4kar@gmail.com>
2019-06-13 21:39:12 +0300

    Add test with PageObject

--- sichkarProj/src/test/java/loginTest/LoginTest.java
+++ sichkarProj/src/test/java/loginTest/LoginTest.java
@@ -1,24 +1,30 @@
 package loginTest;
 
+import org.junit.After;
 import org.junit.Assert;
+import org.junit.Before;
 import org.junit.Test;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import pages.HomePage;
+
 import java.io.File;
 import java.util.concurrent.TimeUnit;
 
 public class LoginTest {
-    @Test
-    public void validLogin(){
+
+    WebDriver webDriver;
+    @Before
+    public void setUp(){
         File file = new File("/home/user130/Auto//chromedriver");
         System.setProperty("webdriver.chrome.driver", file.getAbsolutePath());
-
-
-        WebDriver webDriver = new ChromeDriver();
-
+        webDriver = new ChromeDriver();
         webDriver.manage().window().maximize();
         webDriver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+     }
+    @Test
+    public void validLogin(){
 
         webDriver.get("http://v3.test.itpmgroup.com");
         webDriver.findElement(By.name("_username")).clear();
@@ -28,7 +34,18 @@ public class LoginTest {
 
         webDriver.findElement(By.tagName("button")).click();
 
-        Assert.assertTrue("Avatar is not present" , webDriver.findElement(By.xpath(".//*[@class='pull-left image']")).isDisplayed());
+        HomePage homePage = new HomePage(webDriver);
+
+        Assert.assertTrue("Avatar is not present" ,
+
+                //webDriver.findElement(By.xpath(".//*[@class='pull-left image']")).isDisplayed()
+
+        homePage.isAvatarPresent()
+        );
+
+            }
+    @After
+    public void tearDown() {
         webDriver.quit();
     }
 }
