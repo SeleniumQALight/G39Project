f4c78e889a1074932055bb2bc39681203b0c5253
Maria <mashakoreniuk@gmail.com>
2019-06-13 14:07:19 +0300

    First test

---
+++ korenukProj/src/test/java/loginTest/LoginTest.java
@@ -0,0 +1,38 @@
+package loginTest;
+
+import org.junit.Assert;
+import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import java.io.File;
+import java.util.concurrent.TimeUnit;
+
+public class LoginTest {
+    @Test
+    public void validLogin(){
+        File file = new File("./src/old_drivers/chromedriver.exe");
+        System.setProperty("webdriver.chrome.driver", file.getAbsolutePath());
+
+        WebDriver webDriver = new ChromeDriver();
+
+        webDriver.manage().window().maximize();
+        webDriver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+
+        webDriver.get("http://v3.test.itpmgroup.com");
+
+        webDriver.findElement(By.name("_username")).clear();
+
+        webDriver.findElement(By.name("_username")).sendKeys("Student");
+
+        webDriver.findElement(By.id("password")).sendKeys("909090");
+
+        webDriver.findElement(By.tagName("button")).click();
+
+        Assert.assertTrue("Avatar is not present",webDriver.findElement(By.xpath(".//*[@class='pull-left image']")).isDisplayed());
+
+           webDriver.quit();
+
+    }
+}
