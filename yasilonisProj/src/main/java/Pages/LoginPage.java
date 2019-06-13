1fa50d745a37bcffe75dbb42ba563f0e8b30de85
Dmitriy <kasar@meta.ua>
2019-06-13 21:36:53 +0300

    Login Test

---
+++ yasilonisProj/src/main/java/Pages/LoginPage.java
@@ -0,0 +1,19 @@
+package Pages;
+
+import org.junit.Assert;
+import org.openqa.selenium.WebDriver;
+
+public class LoginPage extends ParentPage {
+    public LoginPage(WebDriver webDriver) {
+        super(webDriver);
+    }
+    public void openPage(){
+        try {
+            webDriver.get("http://v3.test.itpmgroup.com");
+            logger.info("LoginPage was opened");
+        }catch (Exception e){
+            logger.error("Can not open LoginPage");
+            Assert.fail("Can not open LoginPage");
+        }
+    }
+}
--- yasilonisProj/src/test/java/loginTest/LoginTest.java
+++ yasilonisProj/src/test/java/loginTest/LoginTest.java
@@ -1,6 +1,9 @@
 package loginTest;
 
+import Pages.HomePage;
+import org.junit.After;
 import org.junit.Assert;
+import org.junit.Before;
 import org.junit.Test;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
@@ -10,15 +13,22 @@ import java.io.File;
 import java.util.concurrent.TimeUnit;
 
 public class LoginTest {
-    @Test
-    public void validLogIn () {
+    WebDriver webDriver;
+
+    @Before
+    public void setUp() {
         File file = new File("./src/oldDrivers/chromedriver.exe");
         System.setProperty("webdriver.chrome.driver", file.getAbsolutePath());
 
-        WebDriver webDriver = new ChromeDriver();
+        webDriver = new ChromeDriver();
 
         webDriver.manage().window().maximize();
         webDriver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+    }
+
+    @Test
+    public void validLogIn() {
+
 
         webDriver.get("http://v3.test.itpmgroup.com");
 // .\\*[@name='_username']
@@ -29,9 +39,17 @@ public class LoginTest {
         webDriver.findElement(By.id("password")).sendKeys("909090");
         webDriver.findElement(By.tagName("button")).click();
 
+        HomePage homePage = new HomePage(webDriver);
         Assert.assertTrue("Avatar is not present",
-                webDriver.findElement(By.xpath(".//*[@class='pull-left image']")).isDisplayed());
+//                webDriver.findElement(By.xpath(".//*[@class='pull-left image']")).isDisplayed()
+                homePage.isAvatarPresent()
+        );
 
+        webDriver.quit();
+    }
+
+    @After
+    public void tearDown() {
         webDriver.quit();
     }
 }
