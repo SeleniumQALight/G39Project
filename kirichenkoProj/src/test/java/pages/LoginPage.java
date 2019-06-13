6e169435e2afcd2b0cadacaec9a0e8132e793e24
UnicodMK <unicod.44@gmail.com>
2019-06-13 21:36:50 +0300

    Login test Test cases

---
+++ kirichenkoProj/src/test/java/pages/LoginPage.java
@@ -0,0 +1,23 @@
+package pages;
+
+import org.junit.Assert;
+import org.openqa.selenium.WebDriver;
+
+import java.util.logging.Logger;
+
+public class LoginPage extends ParentPage {
+
+    public LoginPage(WebDriver webDriver) {
+        super(webDriver);
+    }
+
+    public void openPage(){
+        try {
+            webDriver.get("http://v3.test.itpmgroup.com");
+            logger.info("Login Page was opened");
+        }catch (Exception e){
+            logger.error("Can not open Login Page");
+            Assert.fail("Can not open Login Page");
+        }
+    }
+}
