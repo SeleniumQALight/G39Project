4f5e9bb11bfeaa6f6b3473086f2fb0db3cb76e3b
VarvaraKeid <varvarakeyd@gmail.com>
2019-06-17 18:03:46 +0300

    LoginTest_invalidLogin

--- kaidanskaiaProj/src/test/java/loginTest/LoginTest.java
+++ kaidanskaiaProj/src/test/java/loginTest/LoginTest.java
@@ -8,6 +8,7 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import pages.HomePage;
+import pages.LoginPage;
 
 import java.io.File;
 import java.util.concurrent.TimeUnit;
@@ -47,6 +48,27 @@ public class LoginTest {
         Assert.assertTrue("Avatar is not present",
                 //  webDriver.findElement(By.xpath(".//*[@class='pull-left image']")).isDisplayed()
                 homePage.isAvatarPresent());
+
+
+    }
+
+    @Test
+    public  void  invalidLogin(){
+        webDriver.get("http://v3.test.itpmgroup.com");
+
+
+        webDriver.findElement(By.name("_username")).clear();
+        webDriver.findElement(By.name("_username")).sendKeys("Student");
+
+        webDriver.findElement(By.id("password")).clear();
+        webDriver.findElement(By.id("password")).sendKeys("909070");
+
+        webDriver.findElement(By.tagName("button")).click();
+
+        LoginPage loginPage = new LoginPage(webDriver);
+
+        Assert.assertTrue("Login form is not shown",
+                loginPage.isLoginFormPresent());
 
 
     }
