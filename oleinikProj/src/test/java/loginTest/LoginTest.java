6073a1bbd59cfd49edc07f313e4b9802f6acec91
User <Malbox@i.ua>
2019-06-17 15:39:17 +0300

    add Test invalidLogin

--- oleinikProj/src/test/java/loginTest/LoginTest.java
+++ oleinikProj/src/test/java/loginTest/LoginTest.java
@@ -14,37 +14,43 @@ import java.util.concurrent.TimeUnit;
 
 public class LoginTest {
     WebDriver webDriver;
+
     @Before
     public void setUp(){
         File file = new File("./src/old_drivers/chromedriver.exe");
         System.setProperty("webdriver.chrome.driver", file.getAbsolutePath());
-
-         webDriver = new ChromeDriver();
-
+        webDriver = new ChromeDriver();
         webDriver.manage().window().maximize();
         webDriver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
     }
 
     @Test
     public void validLogin(){
-
-
         webDriver.get("http://v3.test.itpmgroup.com");
-
         webDriver.findElement(By.name("_username")).clear();
         webDriver.findElement(By.name("_username")).sendKeys("Student");
-
         webDriver.findElement(By.id("password")).clear();
         webDriver.findElement(By.id("password")).sendKeys("909090");
-
         webDriver.findElement(By.tagName("button")).click();
-
         HomePage homePage = new HomePage(webDriver);
         Assert.assertTrue("Avatar is not present",
 //                webDriver.findElement(By.xpath(".//*[@class='pull-left image']"))
 //                        .isDisplayed()
                 homePage.iaAvatarPresent()
         );
+    }
+
+    @Test
+    public void invalidLogin(){
+        webDriver.get("http://v3.test.itpmgroup.com");
+        webDriver.findElement(By.name("_username")).clear();
+        webDriver.findElement(By.name("_username")).sendKeys("Student");
+        webDriver.findElement(By.id("password")).clear();
+        webDriver.findElement(By.id("password")).sendKeys("9090");
+        webDriver.findElement(By.tagName("button")).click();
+        HomePage homePage = new HomePage(webDriver);
+        Assert.assertFalse(homePage.iaAvatarPresent());
+        System.out.println("Account not found");
     }
 
     @After
