c5b39e4bea3ff36d6886d286795d6838584e31be
Sergii Buchkivskyi <fcmalin@gmail.com>
2019-06-18 21:45:45 +0300

    actions was added

--- buchkovskiiProj/src/main/java/pages/LoginPage.java
+++ buchkovskiiProj/src/main/java/pages/LoginPage.java
@@ -1,13 +1,25 @@
 package pages;
 
 import org.junit.Assert;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
 
 public class LoginPage extends ParentPage{
 
     public LoginPage(WebDriver webDriver) {
         super(webDriver);
     }
+
+    @FindBy (name = "_username")
+    private WebElement inputLogin;
+
+    @FindBy (id = "password")
+    private WebElement inputPassword;
+
+    @FindBy (tagName = "button")
+    private WebElement buttonVhod;
 
     public void openPage(){
         try{
@@ -16,6 +28,47 @@ public class LoginPage extends ParentPage{
         } catch (Exception e){
             logger.error("Can't open LoginPage");
             Assert.fail("Can't open LoginPage");
+        }
+    }
+
+    public void enterTextInToInputLogin(String login) {
+        actionsWithOutElements.enterTextIntoInput(inputLogin, login);
+
+//        try {
+//            //WebElement inputLogin = webDriver.findElement(By.name("_username"));
+//
+//            inputLogin.clear();
+//            inputLogin.sendKeys(login);
+//            logger.info(login + " was imported into input Login");
+//        } catch (Exception e){
+//            logger.error("Can't work with element");
+//            Assert.fail("Can't work with element");
+//        }
+    }
+
+    public void enterTextInToInputPassword(String password) {
+        actionsWithOutElements.enterTextIntoInput(inputPassword, password);
+//        // to-do
+//        try{ //WebElement inputPassword = webDriver.findElement(By.id ("password"));
+//            inputPassword.clear();
+//            inputPassword.sendKeys(password);
+//            // to-do
+//            logger.info(password + " was imported into input Password");
+//        } catch (Exception e){
+//            // to-do
+//            logger.error("Can't work with element");
+//            Assert.fail("Can't work with element");
+//        }
+    }
+
+    public void clickOnButtonVhod() {
+        try {//WebElement buttonVhod = webDriver.findElement(By.tagName("button"));
+            buttonVhod.click();
+            logger.info("Button vhod was clicked");
+        }catch (Exception e){
+            // to-do
+            logger.error("Can't work with element");
+            Assert.fail("Can't work with element");
         }
     }
 }
--- buchkovskiiProj/src/test/java/loginTest/loginTestWithPageObject.java
+++ buchkovskiiProj/src/test/java/loginTest/loginTestWithPageObject.java
@@ -15,30 +15,14 @@ import java.io.File;
 import java.util.concurrent.TimeUnit;
 
 public class loginTestWithPageObject extends ParentTest {
-    WebDriver webDriver;
-    @Before
-    public  void before(){
-        File file = new File("./src/newdrivers/chromedriver.exe");
-        System.setProperty("webdriver.chrome.driver", file.getAbsolutePath());
-
-        webDriver = new ChromeDriver();
-        webDriver.manage().window().maximize();
-        webDriver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
-    }
 
     @Test
     public  void validLogin(){
         loginPage.openPage();
-        HomePage homePage = new HomePage(webDriver);
-        webDriver.findElement(By.name("_username")).clear();
-        webDriver.findElement(By.name("_username")).sendKeys("Student");
-        webDriver.findElement(By.id("password")).clear();
-        webDriver.findElement(By.id("password")).sendKeys("909090");
-        webDriver.findElement(By.tagName("button")).click();
-
-        Assert.assertTrue("Avatar isn't present", homePage.isAvatarPresent());
-                //webDriver.findElement(By.xpath(".//*[@class='pull-left image']")).isDisplayed()
-
+        loginPage.enterTextInToInputLogin("Student");
+        loginPage.enterTextInToInputPassword("909090");
+        loginPage.clickOnButtonVhod();
+        checkExpectedResult("Avatar is not pressent", true, homePage.isAvatarPresent());
     }
 
     @Test
@@ -48,18 +32,11 @@ public class loginTestWithPageObject extends ParentTest {
         webDriver.findElement(By.name("_username")).clear();
         webDriver.findElement(By.name("_username")).sendKeys("Student");
         webDriver.findElement(By.id("password")).clear();
-        webDriver.findElement(By.id("password")).sendKeys("909090");
+        webDriver.findElement(By.id("password")).sendKeys("000000");
         webDriver.findElement(By.tagName("button")).click();
 
-        Assert.assertTrue("Avatar isn't present", homePage.isAvatarPresent());
+        Assert.assertFalse("Avatar isn't present", homePage.isAvatarPresent());
         //webDriver.findElement(By.xpath(".//*[@class='pull-left image']")).isDisplayed()
-
     }
-
-    @After
-    public void  after(){
-        webDriver.quit();
-    }
-
 
 }
