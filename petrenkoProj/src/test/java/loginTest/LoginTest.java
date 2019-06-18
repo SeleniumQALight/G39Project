f0b13a11097dced781a12119ba8c4993c228b18f
YuraPetrenko <bald2003@ukr.net>
2019-06-18 19:28:35 +0300

    first test
    Signed-off-by: YuraPetrenko <bald2003@ukr.net>

--- petrenkoProj/src/test/java/loginTest/LoginTest.java
+++ petrenkoProj/src/test/java/loginTest/LoginTest.java
@@ -1,25 +1,37 @@
 package loginTest;
 
+import org.junit.After;
 import org.junit.Assert;
+import org.junit.Before;
 import org.junit.Test;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import pages.HomePage;
+
 
 import java.io.File;
 import java.util.concurrent.TimeUnit;
 
 public class LoginTest {
+    WebDriver  webDriver;
+    @Before
+
+    public void  setup(){
+        File file = new File("./src/old_drivers/chromedriver.exe");
+        System.setProperty("webdriver.chrome.driver", file.getAbsolutePath());
+        webDriver = new ChromeDriver();
+
+        webDriver.manage().window().maximize();
+        webDriver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+    }
+
+
 
     @Test
 
     public void validLogIn(){
-        File file = new File("./src/old_drivers/chromedriver.exe");
-        System.setProperty("webdriver.chrome.driver", file.getAbsolutePath());
-        WebDriver  webDriver = new ChromeDriver();
 
-        webDriver.manage().window().maximize();
-        webDriver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
 
         webDriver.get("http://v3.test.itpmgroup.com");
         webDriver.findElement(By.name("_username")).clear();
@@ -29,11 +41,11 @@ public class LoginTest {
         webDriver.findElement(By.id("password")).sendKeys("909090");
 
         webDriver.findElement(By.tagName("button")).click();
-
-        Assert.assertTrue("Avatar is not present",
-                webDriver.findElement(By.xpath(".//*[@class='pull-left image']")).isDisplayed());
-        webDriver.quit();
-
+        HomePage homePage = new HomePage(webDriver);
+        Assert.assertTrue("Avatar is not present",homePage.isAvatarPresent()
+              //  webDriver.findElement(By.xpath(".//*[@class='pull-left image']")).isDisplayed());
+        //webDriver.quit();
+        );
 
 
 
@@ -41,7 +53,11 @@ public class LoginTest {
 
     }
 
+    @After
+public void tearDown(){
+        webDriver.quit();
 
+}
 
 
 }
