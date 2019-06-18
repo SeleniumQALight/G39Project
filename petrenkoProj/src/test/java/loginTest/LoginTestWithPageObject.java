067b95c41ec6b01f4579d912f7e6f4491db9dd0b
YuraPetrenko <bald2003@ukr.net>
2019-06-18 19:21:54 +0300

    Разнос по папкам и классам
    Signed-off-by: YuraPetrenko <bald2003@ukr.net>

---
+++ petrenkoProj/src/test/java/loginTest/LoginTestWithPageObject.java
@@ -0,0 +1,13 @@
+package loginTest;
+
+import org.junit.Test;
+import parentTest.ParentTest;
+
+public class LoginTestWithPageObject extends ParentTest {
+
+    @Test
+
+    public void validLogin(){
+    loginPage.openPage();
+    }
+}
