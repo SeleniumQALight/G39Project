da213218d42b5e6b6a8737f406e117a272cc0297
YuraPetrenko <bald2003@ukr.net>
2019-06-18 20:02:02 +0300

    first test
    Signed-off-by: YuraPetrenko <bald2003@ukr.net>

--- petrenkoProj/src/main/java/pages/ParentPage.java
+++ petrenkoProj/src/main/java/pages/ParentPage.java
@@ -7,7 +7,6 @@ import java.sql.Driver;
 
 abstract public class ParentPage {
      WebDriver webDriver;
-
      Logger logger = Logger.getLogger(getClass());
 
     public ParentPage(WebDriver webDriver) {
