59e53e4f2911a79c1a62ea0d03e0219aaa582912
Sergii Buchkivskyi <fcmalin@gmail.com>
2019-07-04 20:44:58 +0300

    HtmlElements + Parameters

--- buchkovskiiProj/src/main/java/pages/ParentPage.java
+++ buchkovskiiProj/src/main/java/pages/ParentPage.java
@@ -4,8 +4,12 @@ import libs.ActionsWithOutElements;
 import org.apache.log4j.Logger;
 import org.junit.Assert;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.PageFactory;
 import org.openqa.selenium.support.ui.ExpectedCondition;
+import ru.yandex.qatools.htmlelements.loader.decorator.HtmlElementDecorator;
+import ru.yandex.qatools.htmlelements.loader.decorator.HtmlElementLocatorFactory;
+
 import java.util.regex.Pattern;
 
 abstract public class ParentPage {
@@ -18,7 +22,8 @@ abstract public class ParentPage {
     public ParentPage(WebDriver webDriver, String partURL) {
 
         this.webDriver = webDriver;
-        PageFactory.initElements(webDriver, this);
+        //PageFactory.initElements(webDriver, this);
+        PageFactory.initElements(new HtmlElementDecorator(new HtmlElementLocatorFactory(webDriver)), this);
         actionsWithOutElements = new ActionsWithOutElements(webDriver);
         expectedURL = BASE_URL+partURL;
     }
