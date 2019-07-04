1670ee20402a3e62ca4e8b253defe34d02da8505
yuliiachernysh <chernysh1993@gmail.com>
2019-07-04 21:35:38 +0300

    base url

--- chernishProj/src/test/java/pages/ParentPage.java
+++ chernishProj/src/test/java/pages/ParentPage.java
@@ -1,6 +1,8 @@
 package pages;
 
 import libs.ActionsWithOurElements;
+import libs.ConfigProperties;
+import org.aeonbits.owner.ConfigFactory;
 import org.apache.log4j.Logger;
 import org.junit.Assert;
 import org.openqa.selenium.WebDriver;
@@ -16,15 +18,18 @@ abstract public class ParentPage {
     Logger logger = Logger.getLogger(getClass());
     ActionsWithOurElements actionWithOurElements;
 
-    final String BASE_URL = "http://v3.test.itpmgroup.com";
+    protected static ConfigProperties configProperties = ConfigFactory.create(ConfigProperties.class);
+
+    String baseUrl;
     String expectedUrl;
 
     public ParentPage(WebDriver webDriver, String partUrl) {
         this.webDriver = webDriver;
+        baseUrl = configProperties.base_url();
         //PageFactory.initElements(webDriver, this);
         PageFactory.initElements(new HtmlElementDecorator(new HtmlElementLocatorFactory(webDriver)), this);
         actionWithOurElements = new ActionsWithOurElements(webDriver);
-        expectedUrl = BASE_URL + partUrl;
+        expectedUrl = baseUrl + partUrl;
 
     }
 
