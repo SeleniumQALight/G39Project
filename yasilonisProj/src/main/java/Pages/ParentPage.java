44add992e8fd0d85c4438f32338406a26fe51500
Dmitriy <kasar@meta.ua>
2019-07-09 21:47:56 +0300

    Work with Excel + firefox

--- yasilonisProj/src/main/java/Pages/ParentPage.java
+++ yasilonisProj/src/main/java/Pages/ParentPage.java
@@ -1,6 +1,8 @@
 package Pages;
 
 import libs.ActionsWithOurElements;
+import libs.ConfigProperties;
+import org.aeonbits.owner.ConfigFactory;
 import org.apache.log4j.Logger;
 import org.junit.Assert;
 import org.openqa.selenium.WebDriver;
@@ -10,6 +12,10 @@ abstract public class ParentPage {
     WebDriver webDriver;
     Logger logger = Logger.getLogger(getClass());
     ActionsWithOurElements actionsWithOurElements;
+
+    public static ConfigProperties configProperties =
+            ConfigFactory.create(ConfigProperties.class);
+
     final String BASE_URL = "http://v3.test.itpmgroup.com";
     String expectedUrl;
 
