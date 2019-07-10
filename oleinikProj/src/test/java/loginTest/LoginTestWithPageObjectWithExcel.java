77832ed05a816f82745c8d0816d0c013780e54a4
User <Malbox@i.ua>
2019-07-10 21:08:49 +0300

    LoginTestWithPageObjectWithExcel

---
+++ oleinikProj/src/test/java/loginTest/LoginTestWithPageObjectWithExcel.java
@@ -0,0 +1,29 @@
+package loginTest;
+
+import libs.ConfigProperties;
+import libs.ExcelDriver;
+import org.junit.Test;
+import parentTest.ParentTest;
+
+import java.io.IOException;
+import java.util.Map;
+
+import static pages.ParentPage.configProperties;
+
+public class LoginTestWithPageObjectWithExcel extends ParentTest {
+
+    @Test
+    public void validLogin() throws IOException {
+        ExcelDriver excelDriver = new ExcelDriver();
+        Map dataForValidLogin = excelDriver.getData(configProperties.DATA_FILE(),"validLogOn");
+        loginPage.openPage();
+        loginPage.enterTextInToInputLogin(dataForValidLogin.get("login").toString());
+        loginPage.enterTextInToInputPass(dataForValidLogin.get("pass").toString());
+        loginPage.clickOnButtonVhod();
+
+        checkExpectedResult("Avatar is not present", true, homePage.isAvatarPresent());
+
+
+    }
+
+}
