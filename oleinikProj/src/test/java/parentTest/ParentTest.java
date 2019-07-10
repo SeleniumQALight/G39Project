c5748a4713ed186801cfe7b9ea3ab9d08c6abacb
User <Malbox@i.ua>
2019-07-10 21:11:13 +0300

    firefox browser

--- oleinikProj/src/test/java/parentTest/ParentTest.java
+++ oleinikProj/src/test/java/parentTest/ParentTest.java
@@ -5,6 +5,8 @@ import org.junit.Assert;
 import org.junit.Before;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.firefox.FirefoxOptions;
 import pages.*;
 
 import java.io.File;
@@ -12,7 +14,9 @@ import java.util.concurrent.TimeUnit;
 
 public class ParentTest {
     WebDriver webDriver;
-   protected LoginPage loginPage;
+    String browser = System.getProperty("browser");
+
+    protected LoginPage loginPage;
     protected HomePage homePage;
     protected SparesPage sparesPage;
     protected EditSparePage editSparePage;
@@ -21,10 +25,19 @@ public class ParentTest {
 
     @Before
     public void setUp(){
-        File file = new File("./src/old_drivers/chromedriver.exe");
-        System.setProperty("webdriver.chrome.driver", file.getAbsolutePath());
+        if(browser == null || "chrome".equals(browser.toLowerCase())) {
+            File file = new File("./src/old_drivers/chromedriver.exe");
+            System.setProperty("webdriver.chrome.driver", file.getAbsolutePath());
 
-        webDriver = new ChromeDriver();
+            webDriver = new ChromeDriver();
+        }else if("firefox".equals(browser.toLowerCase())){
+            File file = new File("./src/drivers/geckodriver.exe");
+            System.setProperty ("webdriver.gecko.driver", file.getAbsolutePath());
+            FirefoxOptions profile = new FirefoxOptions();
+            profile.addPreference("browser.startup.page",0);
+            profile.addPreference("browser.startup.homepage_overdrive.mstone", "ignore");//Suppress the "What's new" page
+            webDriver = new FirefoxDriver();
+        }
 
         webDriver.manage().window().maximize();
         webDriver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
