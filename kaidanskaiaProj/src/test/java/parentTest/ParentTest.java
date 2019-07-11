e27b4862709bd41f152861fb470e2e763ca1d082
VarvaraKeid <varvarakeyd@gmail.com>
2019-07-11 21:39:11 +0300

    WorkWithDB

--- kaidanskaiaProj/src/test/java/parentTest/ParentTest.java
+++ kaidanskaiaProj/src/test/java/parentTest/ParentTest.java
@@ -7,6 +7,9 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.firefox.FirefoxOptions;
+import org.openqa.selenium.ie.InternetExplorerDriver;
+import org.openqa.selenium.remote.CapabilityType;
+import org.openqa.selenium.remote.DesiredCapabilities;
 import pages.*;
 
 import java.io.File;
@@ -34,13 +37,21 @@ public class ParentTest {
             System.setProperty("webdriver.chrome.driver", file.getAbsolutePath());
 
             webDriver = new ChromeDriver();
-        } else if ("fireFox".equals(browser.toLowerCase())){
+        } else if ("firefox".equals(browser.toLowerCase())){
             File file = new File("./src/old_drivers/geckodriver.exe");
             System.setProperty ("webdriver.gecko.driver", file.getAbsolutePath());
             FirefoxOptions profile = new FirefoxOptions();
             profile.addPreference("browser.startup.page",0);
             profile.addPreference("browser.startup.homepage_overdrive.mstone", "ignore");//Suppress the "What's new" page
             webDriver = new FirefoxDriver();
+        } else if ("iedriver".equals(browser)) {
+            File file1 = new File(".././drivers/IEDriverServer.exe");
+            System.setProperty("webdriver.ie.driver", file1.getAbsolutePath());
+            DesiredCapabilities capabilities = DesiredCapabilities.internetExplorer();
+            capabilities.setCapability(InternetExplorerDriver.INTRODUCE_FLAKINESS_BY_IGNORING_SECURITY_DOMAINS, true);
+            capabilities.setCapability("ignoreZoomSetting", true);
+            capabilities.setCapability(CapabilityType.ACCEPT_SSL_CERTS, true);
+            webDriver = new InternetExplorerDriver();
         }
         webDriver.manage().window().maximize();
         webDriver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
