da29eb534a3c684735ae83b6865a42197c94be85
UnicodMK <unicod.44@gmail.com>
2019-07-11 21:39:48 +0300

    HTML Elements and params + firefox+ excel+work whith DB

--- kirichenkoProj/src/test/java/parentTest/ParentTest.java
+++ kirichenkoProj/src/test/java/parentTest/ParentTest.java
@@ -40,6 +40,14 @@ public class ParentTest {
             profile.addPreference("browser.startup.page",0);
             profile.addPreference("browser.startup.homepage_overdrive.mstone", "ignore");//Suppress the "What's new" page
             webDriver = new FirefoxDriver();
+       /* }else if ("iedriver".equals(browser)) {
+            File file1 = new File("./src/old_drivers/IEDriverServer.exe");
+            System.setProperty("webdriver.ie.driver", file1.getAbsolutePath());
+            DesiredCapabilities capabilities = DesiredCapabilities.internetExplorer();
+            capabilities.setCapability(InternetExplorerDriver.INTRODUCE_FLAKINESS_BY_IGNORING_SECURITY_DOMAINS, true);
+            capabilities.setCapability("ignoreZoomSetting", true);
+            capabilities.setCapability(CapabilityType.ACCEPT_SSL_CERTS, true);
+            webDriver = new InternetExplorerDriver();   */
         }
 
         webDriver.manage().window().maximize();
