16a79c073e1bae32f344cf5cd5f4e59890fbcde4
Dinamovec <a.si4kar@gmail.com>
2019-07-18 21:47:02 +0300

    Add Allure Report

---
+++ sichkarProj/src/test/java/suits/SmokeSuit.java
@@ -0,0 +1,20 @@
+package suits;
+
+import loginTest.LoginTestWithPageObject;
+import loginTest.UnValidLoginWithParams;
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+import testDB.TestDB;
+
+@RunWith(Suite.class)
+@Suite.SuiteClasses(
+        {
+                LoginTestWithPageObject.class,
+                UnValidLoginWithParams.class,
+                TestDB.class
+        }
+)
+
+public class SmokeSuit {
+
+}
