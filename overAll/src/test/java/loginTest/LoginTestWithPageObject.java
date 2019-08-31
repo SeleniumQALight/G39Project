a352e618d4770757511de0c8425910df31c9d1b3
Taras <radulenko@gmail.com>
2019-08-31 13:27:01 +0300

    Allure Added

--- overAll/src/test/java/loginTest/LoginTestWithPageObject.java
+++ overAll/src/test/java/loginTest/LoginTestWithPageObject.java
@@ -1,14 +1,25 @@
 package loginTest;
 
+import io.qameta.allure.*;
+import org.junit.Ignore;
 import org.junit.Test;
 import parentTest.ParentTest;
 
+@Epic("Allure examples")
+@Feature("Junit 4 support")
 public class LoginTestWithPageObject extends ParentTest {
     @Test
+    @Description("Some detailed test description")
+    @Story("Base support for bdd annotations")
+    @Link("https://example.org")
+    @Link(name = "allure", type = "mylink")
+    @Issue("123")
+    @Issue("432")
+    @Severity(SeverityLevel.CRITICAL)
     public void validLogin() {
         loginPage.openPage();
         loginPage.enterTextInToInputLogin("Student");
-        loginPage.enterTextInToInputPass("909090");
+        loginPage.enterTextInToInputPass("906090");
         loginPage.clickOnButtonVhod();
 
 //        homePage.checkIsAvatarDisplayed();
